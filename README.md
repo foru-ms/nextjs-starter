16e1d1957d36b152d28b9b19d167e6d52c2198cc
saaduddin <i@saadudd.in>
2024-04-13 09:07:46 -0500

    Update README.md with Foru.ms specific instructions and resources

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Foru.ms](https://foru.ms/) Next.js starter bootstrapped with the minimal endpoints to get you started.
 
 ## Getting Started
+
+First, we need to populate the .env file with the following variables:
+
+```bash
+FORU_MS_API_KEY="YOUR_API_KEY"
+FORU_MS_API_URL="https://foru.ms/api/v1"
+```
+
+To obtain an API key, you need to create an account on [Foru.ms](https://foru.ms/) and create a new instance. You can find the API key in the instance settings.
+
+## Running locally
 
 First, run the development server:
 
@@ -14,27 +25,19 @@ pnpm dev
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Open [http://localhost:3000](http://localhost:3000) with your browser to view the app.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+## Deploying on Vercel
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/) from the creators of Next.js.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fforu-ms%2Fnextjs-starter&env=FORU_MS_API_KEY,FORU_MS_API_URL&envDescription=API%20Key%20and%20URL%20needed%20for%20the%20application.&envLink=https%3A%2F%2Fforu.ms&demo-title=Demo%20Foru.ms&demo-description=A%20functional%20forum%20example%20using%20Next.js.&demo-url=https%3A%2F%2Fforu-ms-starter.vercel.app%2F&demo-image=https%3A%2F%2Fforu-ms-starter.vercel.app%2Fdemo.png)
 
 ## Learn More
 
-To learn more about Next.js, take a look at the following resources:
+To learn more about Foru.ms, take a look at the following resources:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [Foru.ms Documentation](https://foru.ms/docs) - learn about Foru.ms features and API.
+- [Blog](https://foru.ms/blog) - read the latest news and updates.
+- [GitHub](https://github.com/foru-ms) - check out the Foru.ms GitHub repositories.
+- [Support](https://foru.ms/support) - get help and support.
--- hooks/data/useForumsApi.js
+++ hooks/data/useForumsApi.js
@@ -1,6 +1,6 @@
 const useForumsApi = () => {
     const fetchUsers = async (page) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/users?page=${page}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/users?page=${page}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -11,7 +11,7 @@ const useForumsApi = () => {
     };
 
     const registerUser = async (username, email, password) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/auth/register`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/auth/register`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -24,7 +24,7 @@ const useForumsApi = () => {
     };
 
     const loginUser = async (email, password) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/auth/login`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -37,7 +37,7 @@ const useForumsApi = () => {
     };
 
     const updateUser = async (id, username, email, password) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/user/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/user/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -50,7 +50,7 @@ const useForumsApi = () => {
     };
 
     const fetchUser = async (token) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/auth/me`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/auth/me`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -62,7 +62,7 @@ const useForumsApi = () => {
     };
 
     const deleteUser = async (id) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/user/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/user/${id}`, {
             method: 'DELETE',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -73,7 +73,7 @@ const useForumsApi = () => {
     };
 
     const fetchThreads = async (page = 1) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/threads?page=${page}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/threads?page=${page}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -84,7 +84,7 @@ const useForumsApi = () => {
     };
 
     const createThread = async (title, body, userId) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/thread`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/thread`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -97,7 +97,7 @@ const useForumsApi = () => {
     };
 
     const fetchThread = async (id) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/thread/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/thread/${id}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -108,7 +108,7 @@ const useForumsApi = () => {
     };
 
     const updateThread = async (id, title, body) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/thread/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/thread/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -121,7 +121,7 @@ const useForumsApi = () => {
     };
 
     const deleteThread = async (id) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/thread/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/thread/${id}`, {
             method: 'DELETE',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -132,7 +132,7 @@ const useForumsApi = () => {
     };
 
     const fetchThreadPosts = async (threadId, page = 1) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/thread/${threadId}/posts?page=${page}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/thread/${threadId}/posts?page=${page}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -143,7 +143,7 @@ const useForumsApi = () => {
     };
 
     const fetchPosts = async (page = 1) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/posts?page=${page}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/posts?page=${page}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -154,7 +154,7 @@ const useForumsApi = () => {
     };
 
     const createPost = async (body, threadId, userId) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/post`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/post`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -167,7 +167,7 @@ const useForumsApi = () => {
     };
 
     const fetchPost = async (id) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/post/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/post/${id}`, {
             method: 'GET',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -178,7 +178,7 @@ const useForumsApi = () => {
     };
 
     const updatePost = async (id, body) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/post/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/post/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -191,7 +191,7 @@ const useForumsApi = () => {
     };
 
     const deletePost = async (id) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/post/${id}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/post/${id}`, {
             method: 'DELETE',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
@@ -202,7 +202,7 @@ const useForumsApi = () => {
     };
 
     const search = async (query, type, page = 1) => {
-        const response = await fetch(`${process.env.FORU_MS_API_URL}/api/v1/search/${query}?type=${type}&page=${page}`, {
+        const response = await fetch(`${process.env.FORU_MS_API_URL}/search/${query}?type=${type}&page=${page}`, {
             method: 'POST',
             headers: {
                 'x-api-key': process.env.FORU_MS_API_KEY,
