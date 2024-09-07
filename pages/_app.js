d3fd11df624ac0d975c831c9195c3c34d55518fe
saaduddin <i@saadudd.in>
2024-09-07 10:12:29 -0500

    Add nextjs-toploader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
+import { PagesTopLoader } from 'nextjs-toploader/pages';
 import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
   return (<>
+  <PagesTopLoader color="rgb(29 78 216)" />
   <Toaster />
   <Component {...pageProps} />
   </>
